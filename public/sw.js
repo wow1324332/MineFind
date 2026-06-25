c3ff466b3d428e50876c5ff4ca6976fe8b0bdab4
wow1324332 <wow1324332@gmail.com>
2026-06-25 13:43:49 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,8 @@
+// public/sw.js
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
 });
 
 self.addEventListener('fetch', (e) => {
-  // PWA 설치 조건을 만족하기 위한 최소한의 fetch 이벤트 리스너
+  // PWA 설치 조건을 만족시키기 위한 더미 fetch 이벤트
 });
