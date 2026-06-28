06fe602054cf39de1ae784de1ba39af73b2de555
wow1324332 <wow1324332@gmail.com>
2026-06-28 17:48:29 +0900

    Update background source for FIRE_DUNGEON_LOADING

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const SPLASH_CONFIG = {
   FIRE_DUNGEON_LOADING: {
     message: "불의 던전으로 강습 중...",
     logoSrc: "/huntlistloading-logo.png", 
-    bgSrc: "/hellofflameloading.jpg", 
+    bgSrc: "/hellofflameloading-bg.jpg", 
     bgOpacity: "opacity-90",
     disablePulse: true,
     useFadeIn: true
