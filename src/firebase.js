336e3572331067e809d2f644e242c626e5024b23
wow1324332 <wow1324332@gmail.com>
2026-07-04 10:37:57 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// 💡 [추가] 데이터베이스(Firestore) 기능을 사용하기 위해 불러옵니다.
+import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyACKHLXCfL-5gg-ZOTP62S6Uzs3ZI46ykQ",
@@ -15,3 +17,6 @@ const app = initializeApp(firebaseConfig);
 
 // Auth 모듈 외부에서 사용할 수 있도록 export
 export const auth = getAuth(app);
+
+// 💡 [추가] MyPage 등 다른 컴포넌트에서 데이터베이스를 사용할 수 있도록 진짜 db를 생성하고 export 합니다!
+export const db = getFirestore(app);
