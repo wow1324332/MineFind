4ae4d3e1a616a2dd62cd5acd06f9e93df1c7eb9d
wow1324332 <wow1324332@gmail.com>
2026-07-10 23:30:06 +0900

    Update Knights.jsx

--- src/components/Knights.jsx
+++ src/components/Knights.jsx
@@ -188,22 +188,6 @@ export default function Knights({ onBack, hp }) {
             </button>
           </div>
 
-          <div className="absolute left-1/2 -translate-x-1/2 flex items-center gap-[4px] drop-shadow-md z-20 pointer-events-none">
-            {[...Array(5)].map((_, i) => (
-              <img 
-                key={i} 
-                src="/hpball.png" 
-                alt="HP" 
-                className={`w-[18px] h-[18px] object-contain transition-all duration-500 ${
-                  i < hp 
-                    ? 'opacity-100 drop-shadow-[0_0_5px_rgba(220,38,38,0.95)]' 
-                    : 'opacity-20 grayscale saturate-50'
-                }`} 
-                draggable="false"
-              />
-            ))}
-          </div>
-
           <div className="flex-1 flex flex-col justify-end p-5 pb-8">
             <div className="mb-4 animate-[slideUp_0.4s_ease-out] flex justify-between items-end">
               <div>
