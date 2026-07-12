f6236a72863639bc9204ba4d7d78ee34ba22c83e
wow1324332 <wow1324332@gmail.com>
2026-07-13 08:27:55 +0900

    Create titleData.js

---
+++ src/constants/titleData.js
@@ -0,0 +1,34 @@
+export const TITLE_DATABASE = {
+  novice: {
+    id: 'novice',
+    name: '무명의 용사',
+    description: '이제 막 시공의 틈에 발을 들인 풋내기',
+    conditionText: '기본 지급',
+    textColor: 'text-neutral-400',
+    glow: ''
+  },
+  fire_survivor: {
+    id: 'fire_survivor',
+    name: '업화의 생존자',
+    description: '불의 던전을 정복하고 살아남은 자',
+    conditionText: '불의 던전 10회 클리어',
+    textColor: 'text-red-500',
+    glow: 'drop-shadow-[0_0_8px_rgba(220,38,38,0.8)]'
+  },
+  rich_goblin: {
+    id: 'rich_goblin',
+    name: '황금 고블린',
+    description: '재물에 눈이 멀어 광산을 떠도는 영혼',
+    conditionText: '누적 10,000 골드 획득',
+    textColor: 'text-yellow-400',
+    glow: 'drop-shadow-[0_0_8px_rgba(250,204,21,0.8)]'
+  },
+  knight_lord: {
+    id: 'knight_lord',
+    name: '위대한 기사단장',
+    description: '수많은 영웅들을 이끄는 전설적인 지휘관',
+    conditionText: '기사단 6명 소환 달성',
+    textColor: 'text-[#f5d5a9]', // 밝은 베이지 골드
+    glow: 'drop-shadow-[0_0_10px_rgba(216,180,134,1)]'
+  }
+};
