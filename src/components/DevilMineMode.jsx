5e8ec78bc8b0a64ee696cc44cd28e4fdc0f9a293
wow1324332 <wow1324332@gmail.com>
2026-07-13 17:06:58 +0900

    Update DevilMineMode.jsx

--- src/components/DevilMineMode.jsx
+++ src/components/DevilMineMode.jsx
@@ -55,7 +55,7 @@ export default function DevilMineMode({ onSelectPVE, onBack, onLogout, hp }) {
             className="transition-all duration-150 brightness-90 saturate-90 active:scale-90 active:brightness-75 drop-shadow-[0_4px_6px_rgba(0,0,0,0.8)] px-2 select-none"
             style={{ WebkitTapHighlightColor: 'transparent', outline: 'none' }}
           >
-            <img src="/backkey.png" alt="Back" className="w-8 h-8 object-contain pointer-events-none" draggable="false" />
+            <img src="/header/backkey.webp" alt="Back" className="w-8 h-8 object-contain pointer-events-none" draggable="false" />
           </button>
 
           <div className="absolute left-1/2 -translate-x-1/2 flex items-center gap-[4px] drop-shadow-md z-20 pointer-events-none">
