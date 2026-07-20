a62fcc597ba2a51d35f600b90608de944a8172a3
wow1324332 <wow1324332@gmail.com>
2026-07-20 21:17:35 +0900

    Modify loading message for Hell of Flame dungeon
    Updated the loading message for the Hell of Flame dungeon.

--- src/constants/dungeonData.js
+++ src/constants/dungeonData.js
@@ -7,7 +7,7 @@ export const DUNGEON_INFO = {
     buttonImg: '/dungeons/fire/hellofflame-bt.webp',
     titleImg: '/dungeons/fire/hellofflame-title.webp',
     loadingBg: '/dungeons/fire/hellofflameloading-bg.webp',
-    loadingMsg: "함펠의 봉인 된 유적에서\n화염의 대악마\n메피스토가 깨어났다\n그의 사악한 불길에\n맞서야만 한다..",
+    loadingMsg: "지옥의 겁화속\n화염의 대악마 메피스토...",
     loadingLogo: "/dungeons/fire/hellofflame-bt.webp",
     loadingOpacity: "opacity-70",
     winBg: '/dungeons/fire/hellofflamewin.webp',
