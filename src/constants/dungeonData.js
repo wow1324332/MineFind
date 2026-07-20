92a80ddc14af1a165d0c7652c162950cf80173ca
wow1324332 <wow1324332@gmail.com>
2026-07-20 21:05:02 +0900

    Update dungeonData.js

--- src/constants/dungeonData.js
+++ src/constants/dungeonData.js
@@ -7,7 +7,7 @@ export const DUNGEON_INFO = {
     buttonImg: '/dungeons/fire/hellofflame-bt.webp',
     titleImg: '/dungeons/fire/hellofflame-title.webp',
     loadingBg: '/dungeons/fire/hellofflameloading-bg.webp',
-    loadingMsg: "불의 던전으로 강습 중...",
+    loadingMsg: "함펠의 봉인 된 유적에서/n악의화신, 화염의 대악마 메피스토가 깨어났다/n함펠의 높다른 성벽이 무너지나니...",
     loadingLogo: "/dungeons/fire/hellofflame-bt.webp",
     loadingOpacity: "opacity-70",
     winBg: '/dungeons/fire/hellofflamewin.webp',
